957163666ac8fe0a042271a09bce3613b842e359
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-06-27 16:02:43 +0100

    fix: update screencontainer to pass the rest of safeareaview props

--- components/atoms/ScreenContainer.tsx
+++ components/atoms/ScreenContainer.tsx
@@ -2,24 +2,24 @@ import { memo } from 'react';
 
 import { LinearGradient } from 'expo-linear-gradient';
 import { cssInterop } from 'nativewind';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ViewProps } from 'react-native';
 
 import { getColor } from '@/utils/getColor';
 
-type ScreenContainerProps = {
+interface ScreenContainerProps extends ViewProps {
   children: React.ReactNode;
-};
+}
 
 const StyledGradient = cssInterop(LinearGradient, {
   className: { target: 'style' },
 });
 
 const ScreenContainer: React.FC<ScreenContainerProps> = memo(
-  function ScreenContainer({ children }) {
+  function ScreenContainer({ children, style, ...props }) {
     return (
       <>
         <SafeAreaView className={classes.containerTop} />
-        <SafeAreaView className={classes.container}>
+        <SafeAreaView className={classes.container} style={style} {...props}>
           <StyledGradient
             colors={[getColor('orange-50'), getColor('teal-50')]}
             className="flex-1 items-center justify-center p-6"
