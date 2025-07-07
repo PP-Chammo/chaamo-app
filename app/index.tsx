03b75cb8eae327225d584dbba58fcc6a5d6250f2
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-07-07 11:01:23 +0100

    fix: change splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,23 @@
 import { useEffect } from 'react';
 
+import { Image } from 'expo-image';
 import { router } from 'expo-router';
-import { View } from 'react-native';
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-} from 'react-native-reanimated';
-
-import { Label } from '@/components/atoms';
 
 export default function StartPage() {
-  const scale = useSharedValue(1);
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ scale: scale.value }],
-  }));
-
-  // Animate logo scaling
-  useEffect(() => {
-    scale.value = withRepeat(
-      withTiming(1.1, {
-        duration: 800,
-        easing: Easing.inOut(Easing.ease),
-      }),
-      -1,
-      true,
-    );
-  }, []);
-
-  // Navigate to /sign-in after 2 seconds
+  // Navigate to /onboarding after 7 seconds
   useEffect(() => {
     const timeout = setTimeout(() => {
       router.replace('/screens/onboarding');
-    }, 2000);
+    }, 6000);
 
     return () => clearTimeout(timeout);
   }, []);
 
   return (
-    <View className={classes.container}>
-      <Animated.Image
-        source={require('../assets/images/logo.png')}
-        style={[animatedStyle]}
-      />
-      <Label variant="title" className={classes.logoText}>
-        Chaamo
-      </Label>
-    </View>
+    <Image
+      source={require('../assets/images/splash-screen.gif')}
+      style={{ width: '100%', height: '100%' }}
+      contentFit="contain"
+    />
   );
 }
-
-const classes = {
-  container: 'flex-1 items-center justify-center bg-white',
-  logoText: 'mt-10 text-4xl',
-};
--- app/screens/onboarding.tsx
+++ app/screens/onboarding.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
 import { BlurView } from 'expo-blur';
 import { Link, router } from 'expo-router';
-import { Image, ImageBackground, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, ImageBackground, SafeAreaView, View } from 'react-native';
 
 import { Button, Label } from '@/components/atoms';
+import { Checkbox } from '@/components/molecules';
+import { BooleanChangeParams } from '@/domains/input.types';
 
 export default function OnboardingScreen() {
+  const [form, setForm] = useState({
+    terms: false,
+  });
+
+  const handleChange = ({ name, value }: BooleanChangeParams) => {
+    setForm({ ...form, [name]: value });
+  };
+
   return (
     <SafeAreaView className="flex-1">
       <ImageBackground
@@ -17,8 +28,6 @@ export default function OnboardingScreen() {
             intensity={100}
             tint="light"
             style={{
-              borderTopLeftRadius: 32,
-              borderTopRightRadius: 32,
               overflow: 'hidden',
               padding: 24,
               backgroundColor: 'rgba(255,255,255,0.25)',
@@ -40,26 +49,37 @@ export default function OnboardingScreen() {
               </Label>
             </View>
             <View className="gap-5">
-              <Button onPress={() => router.push('/(auth)/sign-up')}>
+              <Button
+                disabled={!form.terms}
+                onPress={() => router.push('/(auth)/sign-up')}
+              >
                 Signup
               </Button>
               <Button
+                disabled={!form.terms}
                 onPress={() => router.push('/(auth)/sign-in')}
                 variant="primary-light"
               >
                 Login
               </Button>
             </View>
-            <Label className={classes.terms}>
-              By signing up, you agree to CHAAMO&apos;s{' '}
-              <Link className={classes.link} href="/sign-up">
-                Terms of Service
-              </Link>{' '}
-              and acknowledge you&apos;ve read our{' '}
-              <Link className={classes.link} href="/sign-up">
-                Privacy Policy
-              </Link>
-            </Label>
+            <Checkbox
+              className="mt-10"
+              checked={form.terms}
+              onChange={handleChange}
+              name="terms"
+            >
+              <Label className={classes.terms}>
+                By signing up, you agree to CHAAMO&apos;s{' '}
+                <Link className={classes.link} href="/sign-up">
+                  Terms of Service
+                </Link>{' '}
+                and acknowledge you&apos;ve read our{' '}
+                <Link className={classes.link} href="/sign-up">
+                  Privacy Policy
+                </Link>
+              </Label>
+            </Checkbox>
           </BlurView>
         </View>
       </ImageBackground>
@@ -69,5 +89,5 @@ export default function OnboardingScreen() {
 
 const classes = {
   link: 'text-teal-600 underline font-bold',
-  terms: 'text-slate-500 text-md mt-10',
+  terms: 'text-slate-500 text-md',
 };
