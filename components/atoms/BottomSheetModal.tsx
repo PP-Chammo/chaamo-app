0893cfe9fa0e15f11e2c464c3e98ad7784159af5
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-07-22 12:58:42 +0100

    fix: variant bottom sheet

--- components/atoms/BottomSheetModal.tsx
+++ components/atoms/BottomSheetModal.tsx
@@ -23,7 +23,7 @@ interface BottomSheetModalProps {
   height?: number; // Optional custom height
   className?: string;
   overlayClassName?: string;
-  variant?: 'default' | 'secondary';
+  variant?: 'default' | 'primary' | 'secondary';
 }
 
 const screenHeight = Dimensions.get('window').height;
@@ -35,7 +35,7 @@ const BottomSheetModal: React.FC<BottomSheetModalProps> = ({
   height,
   className,
   overlayClassName,
-  variant = 'default',
+  variant = 'primary',
 }) => {
   const insets = useSafeAreaInsets();
   const sheetHeight = height ?? screenHeight * 0.5;
@@ -181,13 +181,15 @@ const classes = {
   overlay: 'absolute inset-0 bg-black/30',
   sheet: {
     base: 'absolute left-0 right-0 bottom-0 rounded-t-3xl shadow-lg',
-    default: 'bg-primary-500',
+    default: 'bg-neutral-500',
+    primary: 'bg-primary-500',
     secondary: 'bg-white',
   },
   handleContainer: 'items-center pt-4 pb-2',
   handle: {
     base: 'w-16 h-1.5 rounded',
-    default: 'bg-white/70',
+    default: 'bg-white',
+    primary: 'bg-white/70',
     secondary: 'bg-slate-600/70',
   },
   content: 'flex-1 justify-center',
