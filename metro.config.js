5a74d516c87d59242dec89579633a7b28e8eeb0b
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-08-19 09:23:52 +0100

    feat: integrate Sentry error monitoring and source map uploads

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const { withNativeWind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 const { transformer, resolver } = config;
 
 config.transformer = {
